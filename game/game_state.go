950496abc273a22bef741b25e38664ecc8ec8db0
ayub <ayub@example.com>
2025-07-14 14:17:48 +0300

    netx

--- game/game_state.go
+++ game/game_state.go
@@ -1,1 +1,12 @@
 package game
+
+type GameState int
+
+// состояние игрового процесса
+const (
+	playing GameState = iota
+	draw
+	crossWin
+	noughtWin
+	quit
+)
--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import "tic_tac_toe/game"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"tic_tac_toe/game"
+)
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	boardSize := 0
+
 	for {
-		if game.InitBoard() {
+		fmt.Print("Enter the size of the board (3-9): ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input.")
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+		boardSize, err = strconv.Atoi(input)
+		if err != nil {
+			//Использовать предыдущий размер по умолчанию
+			boardSize = game.BoardDefaultSize
+		}
+
+		if boardSize < game.BoardMinSize ||
+			boardSize > game.BoardMaxSize {
+			fmt.Println("Invalid board size.")
+		} else {
 			break
 		}
 	}
+
+	board := game.NewBoard(boardSize)
+	player := game.NewPlayer()
+	game := game.NewGame(*board, *player, reader)
 	game.Play()
 }
